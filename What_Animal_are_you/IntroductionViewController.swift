bff621683292e7921bff39f60d27bc978424bbdf
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2021-07-05 20:24:00 -0700

    2021_07_05
    Making Layout ~

---
+++ What_Animal_are_you/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  What_Animal_are_you
+//
+//  Created by 이승용 on 2021/07/05.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ What_Animal_are_youTests/What_Animal_are_youTests.swift
@@ -0,0 +1,33 @@
+//
+//  What_Animal_are_youTests.swift
+//  What_Animal_are_youTests
+//
+//  Created by 이승용 on 2021/07/05.
+//
+
+import XCTest
+@testable import What_Animal_are_you
+
+class What_Animal_are_youTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
